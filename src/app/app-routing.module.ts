c1f0913e5a790d224288cbedec1e74b539b79af5
Huan Nguyen <huannguyen.fit@gmail.com>
2019-05-23 13:16:00 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './page/login/login.component';
-
-import { LayoutSharedModule } from '../app/master-page/layout-shared/layout-shared.module';
-import { HomeComponent } from './page/home/home.component';
-
 const routes: Routes = [
-    { path: '', loadChildren: '../app/master-page/layout-shared/layout-shared.module#LayoutSharedModule'  },
-     { path: '**',   redirectTo: ''},
+    { path: '', loadChildren: '../app/master-page/layout-shared/layout-shared.module#LayoutSharedModule' },
+    { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
